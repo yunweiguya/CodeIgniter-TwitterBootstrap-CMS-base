ef834745b430c2137579a4965afe311d9e795b0a
Julien <julien@piratalondon.com>
2012-12-11 17:22:13 +0000

    Added the latest version of twitter bootstrap

--- deploy/application/views/partials/header.php
+++ deploy/application/views/partials/header.php
@@ -5,7 +5,7 @@
 		
 	<title><?php echo $title ?></title>
 	
-    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css" />
+    <link rel="stylesheet" href="/_includes/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/_includes/css/style.css" />
     
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
@@ -14,33 +14,27 @@
 		google.load('jquery', '1.7.1');
 		//-->
 	</script>
+  <script type="text/javascript" src="/_includes/js/bootstrap.min.js" ></script>
 	<script type="text/javascript" src="/_includes/js/script.js" ></script>
 
 </head>
 
 <body id="<?php echo isset($bodyID) ? $bodyID : ''; ?>">
-
-<div id="wrapper">
-<div id="header">
-	<div style="z-index: 5;" class="topbar-wrapper">
-    <div class="topbar">
-      <div class="topbar-inner">
-        <div class="container">
-          <h3><a href="/"><?php echo $title ?></a></h3>
-          <?php if($loggedIn){ ?>
-          <ul class="nav">
-            <li class="<?php echo ($bodyID == 'home-index') ? 'active' : ''; ?>"><a href="/">Home</a></li>
-          </ul>         
-          <ul class="nav secondary-nav">
-            <li>
-                <a href="/auth/logout">Logout</a>
-            </li>
-            <?php } ?>
-          </ul>
-        </div>
-      </div><!-- /topbar-inner -->
-    </div><!-- /topbar -->
-  </div>
-</div>
-    <div class="container">
-    
+  <div class="container">
+    <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+          <div class="container">
+            <a class="brand" href="#"><?php echo $title ?></a>
+            <div class="nav-collapse navbar-responsive-collapse collapse" style="height: 0px;">
+              <?php if($loggedIn){ ?>
+              <ul class="nav">
+               <li class="<?php echo ($bodyID == 'home-index') ? 'active' : ''; ?>"><a href="/">Home</a></li>
+              </ul>
+              <ul class="nav pull-right">
+                <li><a href="/auth/logout">Logout</a></li>
+              </ul>
+              <?php } ?>
+            </div><!-- /.nav-collapse -->
+          </div>
+        </div><!-- /navbar-inner -->
+      </div>
